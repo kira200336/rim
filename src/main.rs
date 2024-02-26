9b658d306cdc7ab8729732b94e48f69ba76582d2
Kirubel Midru <kirubelmidru@gmail.com>
2024-02-26 20:03:23 +0300

    Setting terminal display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Write};
+
+use crossterm:: {cursor, terminal, ExecutableCommand, QueueableCommand};
+
+fn main() -> anyhow::Result<()> {
+    let mut stdout = stdout();
+    let mut cx = 0;
+    let mut cy = 0;
+
+    terminal::enable_raw_mode()?;
+    stdout.execute(terminal::EnterAlternateScreen)?;
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    loop {
+        stdout.queue(cursor::MoveTo(cx, cy))?;
+        stdout.flush()?;
+    }
+
+    stdout.execute(terminal::LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
